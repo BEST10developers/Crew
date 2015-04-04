e58c494b5c9dc32f374bac250cd22a3d5c35708d
Kazumasa Kaneko <kazumasa.kaneko@gmail.com>
2015-04-05 02:41:36 +0900

    Add empty test

--- CrewTests/AutoLayoutDSLTest.swift
+++ CrewTests/AutoLayoutDSLTest.swift
@@ -4,11 +4,11 @@
     import AppKit
 #endif
 
-import XCTest
 import Crew
+import XCTest
 
-class CrewTests: XCTestCase {
-    
+class AutoLayoutDSLTest: XCTestCase {
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,17 +18,17 @@ class CrewTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
